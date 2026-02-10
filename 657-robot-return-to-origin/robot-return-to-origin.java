e669f26cc00f549f2321cfa151e45ddff31017ef
Babulu <skbabulu167@gmail.com>
2026-02-10 18:14:23 +0530

    Time: 8 ms (21.34%) | Memory: 45.7 MB (62.71%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int u = 0, d = 0, l = 0, r = 0;
+        for(int i = 0; i < moves.length(); i++){
+            if(moves.charAt(i) == 'U'){
+                u++;
+            }
+            else if(moves.charAt(i) == 'D'){
+                d++;
+            }else if(moves.charAt(i) == 'L'){
+                l++;
+            }else{
+                r++;
+            }
+        }
+        if((u-d) == 0 && (l-r) == 0){
+            return true;
+        }
+        return false;
+    }
+}
