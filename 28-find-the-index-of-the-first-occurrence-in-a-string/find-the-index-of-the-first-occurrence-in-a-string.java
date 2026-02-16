9d37db0988723aaa0fe941c9daea71c519014ac9
Babulu <skbabulu167@gmail.com>
2026-02-16 16:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (96.25%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        /* Method - 1 */ 
         // if(needle.length() > haystack.length()) return -1;
         // for(int i = 0, j = needle.length(); j <= haystack.length(); i++,j++){
         //     if(haystack.substring(i,j).equals(needle)){
@@ -7,9 +8,13 @@ class Solution {
         //     }
         // }
         // return -1;
-        if(haystack.contains(needle)){
-            return haystack.indexOf(needle);
-        }
-        return -1;
+
+        /* Method - 2 */
+        // if(haystack.contains(needle)){
+        //     return haystack.indexOf(needle);
+        // }
+        // return -1;
+        /* Method - 3 */
+        return haystack.indexOf(needle);
     }
 }
