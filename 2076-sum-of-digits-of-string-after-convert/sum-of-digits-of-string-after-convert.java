d9f96a3306f4b9c71eae09a5a081c9bd4cc17301
Babulu <skbabulu167@gmail.com>
2026-08-30 01:12:49 +0530

    Time: 2 ms (52.58%) | Memory: 43.1 MB (48.19%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            sb.append((ch - 'a') + 1);
+        }
+        // K-Times Transform Karna hai
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum = 0;
+            for (char ch : sb.toString().toCharArray()) {
+                sum += ch-'0';
+            }
+            sb = new StringBuilder(String.valueOf(sum));
+            /* First Transform k = 0 par sum = 17 aaya. so, updating sb = 17 
+               Second Transform k = 1 par sum => 1+7 = 8. so, updating sb = 8;
+             */ 
+        }
+        // sb = new StringBuilder(String.valueOf(sum));
+        return sum;
+    }
+}
